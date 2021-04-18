1e99da5279a48988f9da6ead78d232ed1899bb8d
rysamar <ysalcantara.v@gmail.com>
2021-04-18 23:00:50 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function domainGenerator() {
+  let pronoun = ["my", "your", "her", "his", "our"];
+  let adj = ["awesome", "amazing", "annoying", "incredible"];
+  let noun = ["lollipop", "mountain", "sea", "tiger"];
+  let domains = [".es", ".pt", ".ve", ".co", ".ru"];
+  let allTheCombinations = [];
+  let result = "";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  for (let indexPronoun = 0; indexPronoun < pronoun.length; indexPronoun++) {
+    for (let indexAdj = 0; indexAdj < adj.length; indexAdj++) {
+      for (let indexNoun = 0; indexNoun < noun.length; indexNoun++) {
+        for (
+          let indexDomains = 0;
+          indexDomains < domains.length;
+          indexDomains++
+        ) {
+          allTheCombinations.push(
+            result.concat(
+              pronoun[indexPronoun],
+              adj[indexAdj],
+              noun[indexNoun],
+              domains[indexDomains]
+            )
+          );
+        }
+      }
+    }
+  }
+  return allTheCombinations;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function documentInsertion(array) {
+  for (let indexArray = 0; indexArray < array.length; indexArray++) {
+    let containerList = document.querySelector("#domainGeneration");
+    let listElement = document.createElement("li");
+    let textList = document.createTextNode(array[indexArray]);
+    listElement.appendChild(textList);
+    containerList.appendChild(listElement);
+  }
+}
+
+documentInsertion(domainGenerator());
